3a5431f21fd40071b30500942a802d7c22d93b92
Anthony Gedeon <anthony.gedeonn@gmail.com>
2020-01-21 16:02:05 -0600

    added starter files

---
+++ src/scripts/index.js
@@ -0,0 +1,3 @@
+import html from '../../dist/index.html';
+import css from '../../dist/index.css';
+import '../../dist/index.scss';
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    watch: true,
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                     // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+
+            // babel is currently not working
+            // {
+            // test: /\.m?js$/,
+            // exclude: /(node_modules|bower_components)/,
+            // use: {
+            //     loader: 'babel-loader',
+            //     options: {
+            //             presets: ['@babel/preset-env']
+            //         }
+            //     }
+            // },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                  // eslint options (if necessary)
+                },
+            },
+        ],
+    },
+};
