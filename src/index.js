1cfe908b07c8537364a90b790c85763927fac3cf
Anthony Gedeon <anthony.gedeonn@gmail.com>
2020-01-25 10:50:44 -0600

    fixed layout for card component and styled checkboxes but functionality does not work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import html from '../dist/index.html';
 import './assets/stylesheets/index.scss';
+
+const checkboxes = document.querySelectorAll('.checkboxes__input');
+
+function checked() {
+	console.log(this);
+	this.toggleAttribute('checked');
+}
+
+checkboxes.forEach((checkbox) => {
+	checkbox.addEventListener('click', checked);
+});
