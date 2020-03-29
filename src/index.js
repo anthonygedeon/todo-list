e491d8275f721a7d00c0e4a0224c23819127e5c5
anthonygedeon <anthony.gedeonn@gmail.com>
2020-03-29 13:17:48 -0500

    added modules and classes for my todos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-import html from '../dist/index.html';
-import './assets/stylesheets/index.scss';
+const Model = (() => {
 
-const checkboxes = document.querySelectorAll('.checkboxes__input');
+	class TodoList {
+		constructor() {
+			this.todos = [];
+		}
 
-function checked() {
-	console.log(this);
-	this.toggleAttribute('checked');
-}
+		static generateId() {
+			return this.todos.length > 0
+				? this.todos[this.todos.length - 1].id + 1
+				: 0;
+		}
+		
+		addTodo(date, isCompleted, content, categoryList, note) {
+			const todo = {
+				id: this.generateId(),
+				date: date,
+				isCompleted: isCompleted,
+				content: content,
+				categoryList: categoryList,
+				note: note
+			}
 
-checkboxes.forEach((checkbox) => {
-	checkbox.addEventListener('click', checked);
-});
+			this.todos.push(todo)
+		}
+
+		editTodo(id) {
+			
+		}
+
+		deleteTodo(id) {
+
+		}
+	}
+
+
+	return {
+		TodoList
+	};
+})();
+
+const View = (() => {
+	return {
+
+	};
+})();
+
+const Controller = ((model, view) => {
+
+	return {
+
+	}
+})(Model, View);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	entry: './src/index.js',
 	watch: true,
 	output: {
-		filename: 'main.js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
 	module: {
