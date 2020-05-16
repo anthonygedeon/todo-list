d427d0beb19dd201c7dbd46bc4e86f42673e0a48
anthonygedeon <anthony.gedeonn@gmail.com>
2020-05-16 12:17:30 -0500

    added iffes for MVC pattern

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,23 @@
 import '../styles/index.scss';
+import '../images/logo.png';
+
+const model = (() => {
+
+    return {
+
+    }
+})();
+
+const view = (() => {
+
+    return {
+
+    }
+})();
+
+const controller = ((logic, ui) => {
+
+    return {
+        
+    }
+})(model, view);
