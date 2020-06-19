05a1be644d829aa77ea5b3914ed9cd0b42ded286
anthonygedeon <anthony.gedeonn@gmail.com>
2020-06-19 11:21:36 -0500

    The todo part of app works flawlessly, making the project section work wil soon be completed

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,11 +1,16 @@
 const taskCount = document.querySelector('.task-count');
 const taskWrapper = document.querySelector('.tasks');
 const inputTodo = document.querySelector('.new.task');
-const addTodo = document.querySelector('.btn.create');
+const addTodo = document.querySelector('.js-btn-create');
+const inputTodos = document.querySelectorAll('.new.task');
+const clearBtn = document.querySelector('.js-btn-clear');
+
 
 export { 
     taskCount, 
     taskWrapper, 
     inputTodo,
-    addTodo
+    addTodo,
+    inputTodos,
+    clearBtn,
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,19 @@
 const log = console.log;
 
-import { taskCount, taskWrapper, inputTodo, addTodo } from './helpers.js';
+import { taskCount, taskWrapper, inputTodo, addTodo, inputTodos, clearBtn, } from './helpers.js';
 
 const Todo = (() => {
 
-    const collection = [
-        { name: 'Task 1', complete: false, id: 0 },
-        { name: 'Task 2', complete: false, id: 1 },
-        { name: 'Task 3', complete: true, id: 3 },
-        { name: 'Task 4', complete: false, id: 4 },
-        { name: 'Task 5', complete: true, id: 5 },
+    let inputValue;
+    let preventFromAddingTodo = true;
+
+    let collection = [
+        // { name: 'Task 1', complete: false, id: 0 },
+        // { name: 'Task 2', complete: false, id: 1 },
+        // { name: 'Task 3', complete: false, id: 3 },
+        // { name: 'Task 4', complete: false, id: 4 },
+        // { name: 'Task 5', complete: false, id: 5 },
+        // Project { name: 'Youtube', id: 0, todos: [] }
     ];
 
     class Project {
@@ -29,45 +33,118 @@ const Todo = (() => {
 
     const createTodo = (todo) => {
         const task = `
-            <div class="task">
+            <div class="task" data-id="${todo.id}">
                 <input 
                 type="checkbox"
                 id="task-${todo.id}"
                 />
-                <label for="task-${todo.id}">
+                <label for="task-${todo.id}" class="js-todo-item">
                 <span class="custom-checkbox"></span>
                 ${todo.name}
                 </label>
-            </div>`
+            </div>`;
         return task;
     }
+
+    const updateTodoCompletion = (id) => {
+
+        collection.forEach(todo => {
+            if (todo.id == id) {
+                todo.complete = !todo.complete;
+            }
+        })
+        
+        console.log(collection)
+    };
 
     const _displayRemainingTodos = () => {
         taskCount.textContent = `${collection.length} tasks remaining`;
     };
 
-    const _render = () => {
+    const removeTodoFromDOM = () => {
+        Array.from(taskWrapper.children).forEach(todo => todo.remove());
+    }
 
-        _displayRemainingTodos();
+    const removeValueFromInput = () => {
+        inputTodos.forEach(input => input.value = '');
+    };
 
+    const addTodoItem = (todo) => {
+        collection.push(new Todo(todo));
+    } 
+
+    const render = () => {
+        if (collection.length === 0) {
+            return;
+        }
+        
         // render through each todo
         collection.forEach(item => {
             taskWrapper.insertAdjacentHTML('beforeend', createTodo(item))
-        })
+        });
+
     }
 
-    const start = () => {
-        _render();
-    };
-
     inputTodo.addEventListener('input', event => {
-        console.log(event.target.value);
+        inputValue = event.target.value;
+    });
+
+    const start = () => {
+
+        _displayRemainingTodos();
+
+        removeTodoFromDOM();
+
+        render();
+    }
+
+    addTodo.addEventListener('click', () => {
+
+        if (inputTodo.value == '') {
+            preventFromAddingTodo = false;
+        } else {
+            preventFromAddingTodo = true;
+        }
+
+        if (preventFromAddingTodo) {
+
+            addTodoItem(inputValue);
+
+            removeValueFromInput();
+
+            start();
+        }
+    });
+
+    clearBtn.addEventListener('click', () => {
+
+        // clear completed todos
+        collection = collection.filter(todo => todo.complete == false);
+
+        start();
+    });
+    
+    /**
+     * @todo
+     * 1. Handle different areas of click for todo item
+     */
+    window.addEventListener('click', (event) => {
+        if (event.target.classList.contains('custom-checkbox') ) {
+            const id = Number(event.target.parentElement.parentElement.dataset.id);
+            updateTodoCompletion(id);
+        }
+    });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        _displayRemainingTodos();
+
+        start();
     });
 
     return {
-        start
+        render
     }
 
 })();
 
-Todo.start()
+Todo.render();
