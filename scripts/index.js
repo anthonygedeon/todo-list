7754c5276be2028b63f254f846a8dc1a84a2481b
anthonygedeon <anthony.gedeonn@gmail.com>
2020-06-22 18:02:10 -0500

    restart

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,5 @@ import View from './View.js';
 
 const Controller = ((model, view) => {
 
-	document.querySelector('.js-project-add').addEventListener('click', (event) => {
-		event.preventDefault();
-		model.project.addProject(view.ui.projectInputField.value);
-		view.ui.appendProjectComponent();
-		view.ui._removeTextFromInput('.js-project-create');
-	});
-
-	document.querySelector('.task-list').addEventListener('click', (event) => {
-		if (event.target.matches('.list-name')) {
-			view.ui.displayTitle(event.target.textContent);
-		}
-	});
-
-	document.querySelector('.js-btn-clear').addEventListener('click', (event) => {
-
-	});
-
-	return {
-
-	};
 
 })(Model, View);
